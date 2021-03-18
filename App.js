ee8d77f53c7744c2155787a84887402118b125a4
alex17fdez <alex17fdez@gmail.com>
2021-03-18 09:47:06 +0100

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World 2</Text>
+      <Text>Hello World 25</Text>
       <StatusBar style="auto" />
     </View>
   );
